3b9b8cf34cb26ad6d9c56b16b899e614f870fd12
berramos <beaguirre@gmail.com>
2026-08-18 13:42:41 +1000

    Added PWA support with manifest and service worker

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'chess-trainer-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install event: Caches the files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Fetch event: Serves files from cache if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
